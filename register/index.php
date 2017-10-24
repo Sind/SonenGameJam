53354a9bd448f6618effc47dcb0de6eaf8e1d51c
kristebo <krister.borge@gmail.com>
2017-10-24 15:23:42 +0200

    2017

--- register/index.php
+++ register/index.php
@@ -2,14 +2,14 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1;url=http://tinyurl.com/sonengamejamregistrations">
+        <meta http-equiv="refresh" content="1;url=nu link">
         <script type="text/javascript">
-            window.location.href = "http://tinyurl.com/sonengamejamregistrations"
+            window.location.href = "nu link"
         </script>
         <title>Page Redirection</title>
     </head>
     <body>
         <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
-        If you are not redirected automatically, follow <a href='http://tinyurl.com/sonengamejamregistrations'>this link</a>
+        If you are not redirected automatically, follow <a href='nu link'>this link</a>
     </body>
 </html>
